6e17355e5df4e800342743086e02ed72968cbdd7
musharraf ansari <musharrafansari>
2022-06-11 00:59:01 +0530

    changed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,14 +4,33 @@ const UserModel= require("../models/userModel")
 
 
 const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
+    // let tokenDataInHeaders= req.headers.token
+    // console.log(tokenDataInHeaders)
 
     console.log( "HEADER DATA ABOVE")
     console.log( "hey man, congrats you have reached the Handler")
     res.send({ msg: "This is coming from controller (handler)"})
     }
 
+    // const Date = function(req,res,next){
+
+    //     var today = new Date();
+    //     var dd = String(today.getDate()).padStart(2, '0');
+    //     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    //     var yyyy = today.getFullYear();
+        
+    //     today = mm + '/' + dd + '/' + yyyy;
+    //     document.write(today);
+    
+    // let ip=req.ip
+    // console.log(ip)
+    // let url=req.originalurl
+
+    // console.log(url)
+
+    // next()
+        
+    // }
 
 
 
@@ -27,20 +46,17 @@ const basicCode= async function(req, res) {
 
 
 
+// const createUser= async function (req, res) {
+//     let data= req.body
+//     let savedData= await UserModel.create(data)
+//     res.send({msg: savedData})
+// }
 
+// const getUsersData= async function (req, res) {
+//     let allUsers= await UserModel.find()
+//     res.send({msg: allUsers})
+// }
 
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
-}
-
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+// module.exports.createUser= createUser
+// module.exports.getUsersData= getUsersData
 module.exports.basicCode= basicCode
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://musharrafansari:XY5t9CKinqT75evR@cluster0.xsylin5.mongodb.net/musharrafansaridb", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -21,7 +21,36 @@ app.use (
   }
   );
 
-app.use('/', route);
+
+    
+    
+  const musharraf= function(req,res,next){
+
+                var currentdate = new Date(); 
+                var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                                + (currentdate.getMonth()+1)  + "/" 
+                                + currentdate.getFullYear() + " @ "  
+                                + currentdate.getHours() + ":"  
+                                + currentdate.getMinutes() + ":" 
+                                + currentdate.getSeconds();
+     
+    console.log(datetime)
+   
+    let ip=req.ip
+    console.log(ip)
+    let url=req.originalUrl
+    console.log(url)
+    
+
+    // console.log(`${datetime} ${ip} ${url}`)
+
+    
+
+    next()
+        
+    }
+
+    app.use(musharraf)
 
 
 app.listen(process.env.PORT || 3000, function () {
